77b293b49710cf72a8423142fc0d20cd491f9df1
Adarsh Menon <adarsh1021@gmail.com>
2020-05-15 23:50:36 +0530

    added points for hand

--- sketch.js
+++ sketch.js
@@ -44,8 +44,10 @@ function draw() {
   drawVideo();
 }
 
-function drawVideo() {
+async function drawVideo() {
   let scaleFactor = width / capture.width;
+  let scaleX = width / capture.width;
+  let scaleY = height / (2 * capture.height);
   translate(width, 0);
   scale(-1, 1);
   image(
@@ -55,5 +57,22 @@ function drawVideo() {
     width,
     scaleFactor * capture.height
   ); // to fit width
+  if (model) poses = await model.estimateHands(capture.elt);
+  drawKeypoints(scaleX, scaleY);
   translate(width, 0);
+}
+
+function drawKeypoints(scaleX, scaleY) {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+    let pose = poses[i];
+    for (let j = 0; j < pose.landmarks.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.landmarks[j];
+      fill(255, 0, 0);
+      noStroke();
+      ellipse(keypoint[0] * scaleX, keypoint[1] * scaleY + height / 2, 10, 10);
+      // }
+    }
+  }
 }
